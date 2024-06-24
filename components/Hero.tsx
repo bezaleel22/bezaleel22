168bc22b0a11437fdc3a35b88a2f3bd2b419dd10
Onojeta Brown <onosbrown.saved@gmail.com>
2024-06-24 19:50:07 +0100

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Brown, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Brown, a Software Developer based in Nigeria.
           </p>
 
           <a href="#about">
