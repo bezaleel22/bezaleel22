231779901576e64980f82128fe7a9297ae544604
Onojeta Brown <onosbrown.saved@gmail.com>
2024-06-24 14:25:42 +0100

    something

--- next.config.mjs
+++ next.config.mjs
@@ -2,4 +2,10 @@ import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    basePath: ".",
+    images: {
+    unoptimized: true,
+  },
 };
+
+export default nextConfig;
